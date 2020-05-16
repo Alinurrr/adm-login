f63e94d40c089f4a601a90f8e725b5897c06a92d
Alinurrr <alinoer.rachman@gmail.com>
2020-05-16 23:59:13 +0700

    mencoba merubah aplikasi 1 penuh beserta framework ke git

---
+++ application/views/auth/change-password.php
@@ -0,0 +1,43 @@
+<div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-xl-10 col-lg-12 col-md-9">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-2">Change Your Password!</h1>
+                                    <p class="mb-4">for email <?= $this->session->userdata('reset_email');  ?></p>
+                                </div>
+                                <?= $this->session->flashdata('message'); ?>
+                                <form class="user" method="POST" action="<?= base_url('auth/changepassword') ?>">
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="password1" placeholder="Enter New Password..." name="password1">
+                                        <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="password2" placeholder="Reapeat Password..." name="password2">
+                                        <?= form_error('password2', '<small class="text-danger pl-3">', '</small>'); ?>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Change Password
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
